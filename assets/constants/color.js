8f366702c5afb5e04affabc5de79eb7209bb94dc
OmarKoraa <47000537+OmarKoraa@users.noreply.github.com>
2020-08-07 12:31:51 +0200

    Revert "Frequentlies Redesign, Frequently edit and delete bug fix"

--- assets/constants/color.js
+++
@@ -1,4 +0,0 @@
-export const fc = 'purple' //Foreground Color
-export const bc = 'black'  //BackGround Color
-export const bcf = '#111111'  //BackGround Color For ForeGround objects
-export const bcm = '#555555'//Back Ground Color for Modals
